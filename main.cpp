e8ffbfabcbc334103a568dc1e0e10f1dbbbdb9f5
Josh Rivett <josh@rivett.free-online.co.uk>
2018-11-15 14:01:23 +0000

    Added main and makefile
    Nothing in main yet, just for testing.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "babySimulator.h"
+#include "binaryConversion.h"
+
+//Using the standard name space.
+using namespace std;
+
+int main() {
+
+	
+	return 0;
+}
---
+++ makefile
@@ -0,0 +1,2 @@
+babySim: main.cpp babySimulator.cpp binaryConversion.cpp
+	g++ -Wall -Wextra -pedantic -g -o babySim main.cpp binaryConversion.cpp
