904de2dffaf05a262d5893ceb725f2cade734520
Josh Rivett <josh@rivett.free-online.co.uk>
2018-11-15 13:24:23 +0000

    Added cpp file with empty functions + header

---
+++ babySimulator.cpp
@@ -0,0 +1,93 @@
+/*
+	Name: 			Josh Rivett
+	Matric Number: 	170005367
+	Module Code: 	AC21009
+*/
+
+#include <cstdlib>
+#include "babySimulator.h"
+#include "binaryConversion.h"
+
+/* Fetch. */
+int BabySimulator::fetch() {
+
+
+	return SUCCESS;
+}
+		
+/* Decode. */
+int BabySimulator::decode() {
+
+
+	return SUCCESS;
+}
+		
+/* Execute. */
+int BabySimulator::execute() {
+
+
+	return SUCCESS;
+}
+		
+/* Print out. */
+int BabySimulator::printOut() {
+
+
+	return SUCCESS;
+}
+
+/* Increments the control instruction by 1. */
+int BabySimulator::incrementCI() {
+
+
+	return SUCCESS;
+}
+		
+/* Function 0: Copy content of the specified line into the CI. */
+int BabySimulator::JMP() {
+
+
+	return SUCCESS;
+}
+		
+/* Function 1: Add the content of the specified line, negated, into the accumulator. */
+int BabySimulator::JRP() {
+
+
+	return SUCCESS;
+}
+		
+/* Function 2: Copy the content of the specified line, negated, into the accumulator. */
+int	BabySimulator::LDN() {
+
+
+	return SUCCESS;
+}
+		
+/* Function 3: Copy the content of the accumulator to the specified store line. */
+int	BabySimulator::STO() {
+
+
+	return SUCCESS;
+}
+		
+/* Function 4/5: Subtract the content of the specified line from the accumulator. */
+int	BabySimulator::SUB() {
+
+
+	return SUCCESS;
+}
+		
+/* Function 6: If the accumulator is less than 0 increment the CI. */
+int	BabySimulator::CMP() {
+
+
+	return SUCCESS;
+}
+		
+/* Function 7: Halt the Baby. */
+int	BabySimulator::STP() {
+
+
+	return SUCCESS;
+}
---
+++ babySimulator.h
@@ -0,0 +1,56 @@
+/*
+	Name: 			Josh Rivett
+	Matric Number: 	170005367
+	Module Code: 	AC21009
+	References:
+		1 - Header comments for the instruction set taken from here:
+			https://my.dundee.ac.uk/bbcswebdav/pid-5267414-dt-content-rid-2398945_2/courses/AC21009_CAE_D65_201314/SHARP2001GUIDE%281%29.PDF
+*/
+
+enum BabyError { SUCCESS };
+
+class BabySimulator {
+	private:
+		//Declares the fields required by the baby.
+		int store[32][32];		//32 lines, each holding 32 bits.
+		int accumulator[32];	//Holds results of arithmetic operations.
+		int CI;	//Control instruction. Holds line number of next instruction.
+		int PI;	//Present instruction. Holds actual opcode to be executed.
+
+	public:
+		/* Fetch. */
+		int fetch();
+		
+		/* Decode. */
+		int decode();
+		
+		/* Execute, */
+		int execute();
+		
+		/* Print out. */
+		int printOut();
+
+		/* Increments the control instruction by 1. */
+		int incrementCI();
+		
+		/* Function 0: Copy content of the specified line into the CI. */
+		int JMP();
+		
+		/* Function 1: Add the content of the specified line, negated, into the accumulator. */
+		int JRP();
+		
+		/* Function 2: Copy the content of the specified line, negated, into the accumulator. */
+		int	LDN();
+		
+		/* Function 3: Copy the content of the accumulator to the specified store line. */
+		int	STO();
+		
+		/* Function 4/5: Subtract the content of the specified line from the accumulator. */
+		int	SUB();
+		
+		/* Function 6: If the accumulator is less than 0 increment the CI. */
+		int	CMP();
+		
+		/* Function 7: Halt the Baby. */
+		int	STP();
+};
