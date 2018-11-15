e87cfe8280a8387180ffd865ee05fd74e5e4e61c
Josh Rivett <josh@rivett.free-online.co.uk>
2018-11-15 22:26:41 +0000

    Implemented the rest of the instruction set
    Still needs to be tested but the logic should be there

--- babySimulator.cpp
+++ babySimulator.cpp
@@ -46,32 +46,57 @@ int BabySimulator::printOut() {
 
 /* Increments the control instruction by 1. */
 int BabySimulator::incrementCI() {
+	//Adds 1 to the control instruction.
 	CI++;
-	return SUCCESS;
-}
-		
-/* Function 0: Copy content of the specified line into the CI. */
-int BabySimulator::JMP() {
-
 
 	return SUCCESS;
 }
 		
-/* Function 1: Add the content of the specified line, negated, into the accumulator. */
-int BabySimulator::JRP() {
-
-
-	return SUCCESS;
-}
-		
-/* Function 2: Copy the content of the specified line, negated, into the accumulator. */
-int	BabySimulator::LDN() {
-
+/* Function 0: Copy content of the specified line into the CI.
+	line - the line of the store to be manipulated.
+*/
+int BabySimulator::JMP(int line) {
+	//Converts the requested store line to decimal and copies it to the control instruction.
+	CI = BinaryConversion::toDecimal(store[line], storeBits);
 
 	return SUCCESS;
 }
 		
-/* Function 3: Copy the content of the accumulator to the specified store line. */
+/* Function 1: Add the content of the specified line, negated, into the CI.
+	line - the line of the store to be manipulated.
+*/
+int BabySimulator::JRP(int line) {
+	//Converts the requested store line to decimal, negates it, and adds it to the control instruction.
+	CI += -1 * BinaryConversion::toDecimal(store[line], storeBits);
+
+	return SUCCESS;
+}
+		
+/* Function 2: Copy the content of the specified line, negated, into the accumulator.
+	line - the line of the store to be manipulated.
+*/
+int	BabySimulator::LDN(int line) {
+	//Declares the required variables.
+	long decimal;
+	int copy[storeBits];
+
+	//Converts the requested store line to decimal and negates it.
+	decimal = -1 * BinaryConversion::toDecimal(store[line], storeBits);
+
+	//Converts the negated value back to binary.
+	BinaryConversion::toBinary(decimal, copy, storeBits);
+
+	//Loops through the accumulator and copies the negated store line bits to each accumulator bit.
+	for (int i = 0; i < accumulatorSize; i++) {
+		accumulator[i] = copy[i];
+	}
+
+	return SUCCESS;
+}
+		
+/* Function 3: Copy the content of the accumulator to the specified store line.
+	line - the line of the store to be manipulated.
+*/
 int	BabySimulator::STO(int line) {
 	//Loops through the accumulator and stores each of the bits in the requested store line.
 	for (int i = 0; i < accumulatorSize; i++) {
@@ -81,16 +106,37 @@ int	BabySimulator::STO(int line) {
 	return SUCCESS;
 }
 		
-/* Function 4/5: Subtract the content of the specified line from the accumulator. */
-int	BabySimulator::SUB() {
+/* Function 4/5: Subtract the content of the specified line from the accumulator.
+	line - the line of the store to be manipulated.
+*/
+int	BabySimulator::SUB(int line) {
+	//Declares the required variables.
+	long storeValue, accumulatorValue;
 
+	//Converts the requested store line, as well as the accumulator, to decimal values.
+	storeValue = BinaryConversion::toDecimal(store[line], storeBits);
+	accumulatorValue = BinaryConversion::toDecimal(store[line], storeBits);
+
+	//Subtracts the store value from the accumulator value, converts the result back to binary, and stores it in the accumulator.
+	BinaryConversion::toBinary((accumulatorValue-storeValue), accumulator, accumulatorSize);
 
 	return SUCCESS;
 }
 		
-/* Function 6: If the accumulator is less than 0 increment the CI. */
-int	BabySimulator::CMP() {
+/* Function 6: If the accumulator is less than 0 increment the CI.
+	line - the line of the store to be manipulated.
+*/
+int	BabySimulator::CMP(int line) {
+	//Declares the required variables.
+	long accumulatorValue;
 
+	//Converts the accumulator value to a decimal number.
+	accumulatorValue = BinaryConversion::toDecimal(store[line], storeBits);
+
+	//Checks if the accumulator value is less than 0 and if so, increments the control instruction.
+	if (accumulatorValue < 0) {
+		incrementCI();
+	}
 
 	return SUCCESS;
 }
--- babySimulator.h
+++ babySimulator.h
@@ -7,6 +7,9 @@
 			https://my.dundee.ac.uk/bbcswebdav/pid-5267414-dt-content-rid-2398945_2/courses/AC21009_CAE_D65_201314/SHARP2001GUIDE%281%29.PDF
 */
 
+#ifndef BABY_SIMULATOR_H
+#define BABY_SIMULATOR_H
+
 enum BabyError { SUCCESS };
 
 //Declares the required constand values.
@@ -19,8 +22,8 @@ class BabySimulator {
 		//Declares the fields required by the baby.
 		int store[storeLines][storeBits];		//32 lines, each holding 32 bits.
 		int accumulator[accumulatorSize];		//Holds results of arithmetic operations.
-		int CI;	//Control instruction. Holds line number of next instruction.
-		int PI;	//Present instruction. Holds actual opcode to be executed.
+		long CI;	//Control instruction. Holds line number of next instruction.
+		long PI;	//Present instruction. Holds actual opcode to be executed.
 
 	public:
 		/* Constructor. */
@@ -45,23 +48,25 @@ class BabySimulator {
 		int incrementCI();
 		
 		/* Function 0: Copy content of the specified line into the CI. */
-		int JMP();
+		int JMP(int);
 		
 		/* Function 1: Add the content of the specified line, negated, into the accumulator. */
-		int JRP();
+		int JRP(int);
 		
 		/* Function 2: Copy the content of the specified line, negated, into the accumulator. */
-		int	LDN();
+		int	LDN(int);
 		
 		/* Function 3: Copy the content of the accumulator to the specified store line. */
-		int	STO();
+		int	STO(int);
 		
 		/* Function 4/5: Subtract the content of the specified line from the accumulator. */
-		int	SUB();
+		int	SUB(int);
 		
 		/* Function 6: If the accumulator is less than 0 increment the CI. */
-		int	CMP();
+		int	CMP(int);
 		
 		/* Function 7: Halt the Baby. */
 		int	STP();
 };
+
+#endif
