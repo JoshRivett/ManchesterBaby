6abb20abe0eb1cdecb1629035ebac6a0728e8c1a
errrrrrrrrrwhat <37588398+errrrrrrrrrwhat@users.noreply.github.com>
2018-11-18 02:38:10 +0000

    Add files via upload
    basic skeleton, doesn't work yet

---
+++ translate.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+/*
+
+
+void convertToBinary(assemblyCode)
+{
+
+
+
+
+
+
+}
+
+*/
+
+int main()
+{
+convertedMC.open("ConvertedMachineCode.txt");
+
+string line;
+unsigned int curr = 0 //current line position
+while (getline(infile, line)) 
+{
+	curr++;
+	if (line.find(/*something here*/,0)="START") 
+	{
+		convertedMC << "binary here";
+	}
+
+	else if (line.find(/*something here*/, 0) = "")
+	{
+		convertedMC << "binary here";
+	}
+
+	else if (line.find(/*something here*/, 0) = "")
+	{
+		convertedMC << "binary here";
+	}
+
+	else if (line.find(/*something here*/, 0) = "")
+	{
+		convertedMC << "binary here";
+	}
+
+	else if (line.find(/*something here*/, 0) = "")
+	{
+		convertedMC << "binary here";
+	}
+
+	else if (line.find(/*something here*/, 0) = "")
+	{
+		convertedMC << "binary here";
+	}
+	else if (line.find(/*something here*/, 0) = "")
+	{
+		convertedMC << "binary here";
+	}
+
+	else
+	{
+
+	}
+	
+
+}
+
+
+
+
+
+
+
+convertedMC.close();
+}
+
+//https://stackoverflow.com/questions/12463750/c-searching-text-file-for-a-particular-string-and-returning-the-line-number-wh
